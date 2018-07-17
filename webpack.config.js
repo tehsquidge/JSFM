b9e7301387fa38370ab7fa38cb187230604b0f36
liam <liam.d.kerr@gmail.com>
2018-07-17 01:03:59 +0100

    Markedup Modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 var path = require('path');
 
 var config = {
-    entry: './js/src/',
+    entry: './js/src/index.jsx',
     output: {
         path: path.join(__dirname, './www/js/'),
         filename: 'main.js'
     },
     module: {
         rules: [{
-            test: /\.js$/,
+            test: /\.jsx?$/,
             exclude: /node_modules/,
             use: {
                 loader: "babel-loader"
